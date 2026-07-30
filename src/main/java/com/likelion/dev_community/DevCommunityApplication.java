f952829567b41d5683aa22975d7306f688e33428
Noisywhitecat <noisywhitecat@gmail.com>
2026-07-30 10:17:05 +0900

    chore: dev_community 프로젝트 초기 생성

---
+++ src/main/java/com/likelion/dev_community/DevCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.dev_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevCommunityApplication.class, args);
+    }
+
+}
