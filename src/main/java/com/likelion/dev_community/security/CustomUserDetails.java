b95325352cee0edad3b6d517ba95c91ce7982f27
KJH <kjhkmn80@gmail.com>
2026-07-31 11:51:22 +0900

    JwtProvider 각 토큰 생성,파싱 메서드 추가, Jwt인증토큰 및 필터 추가, CustomUserDetails 정의

---
+++ src/main/java/com/likelion/dev_community/security/CustomUserDetails.java
@@ -0,0 +1,68 @@
+package com.likelion.dev_community.security;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final Long userId;
+    private final String username;
+    private final String password;
+    private final String nickname;
+    private final List<GrantedAuthority> authorities;
+
+    public CustomUserDetails(Long userId, String username, String password, String nickname, List<GrantedAuthority> authorities) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+        this.authorities = authorities;
+    }
+
+    public Long getId() {
+        return userId;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
