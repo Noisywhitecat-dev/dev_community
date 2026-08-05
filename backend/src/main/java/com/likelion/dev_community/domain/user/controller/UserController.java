c2bf67da15c53efe87b27654ab340b96ae2d971f
KJH <kjhkmn80@gmail.com>
2026-08-05 11:33:04 +0900

    feat: 회원 정보 조회 API 구현 (F-03)

--- backend/src/main/java/com/likelion/dev_community/domain/user/controller/UserController.java
+++ backend/src/main/java/com/likelion/dev_community/domain/user/controller/UserController.java
@@ -1,9 +1,27 @@
 package com.likelion.dev_community.domain.user.controller;
 
+import com.likelion.dev_community.common.ApiResponse;
+import com.likelion.dev_community.domain.user.dto.userDto.UserInfoResponse;
+import com.likelion.dev_community.domain.user.service.UserService;
+import com.likelion.dev_community.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/members")
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/me")
+    public ResponseEntity<ApiResponse<UserInfoResponse>> getUserInfo(@AuthenticationPrincipal CustomUserDetails customUserDetails){
+        UserInfoResponse userInfo = userService.getUserInfo(customUserDetails.getId());
+
+        return ResponseEntity.ok(ApiResponse.success("회원 정보 조회 성공",userInfo));
+    }
 }
--- backend/src/main/java/com/likelion/dev_community/domain/user/service/UserService.java
+++ backend/src/main/java/com/likelion/dev_community/domain/user/service/UserService.java
@@ -1,7 +1,23 @@
 package com.likelion.dev_community.domain.user.service;
 
+import com.likelion.dev_community.common.exception.CustomException;
+import com.likelion.dev_community.common.exception.ErrorCode;
+import com.likelion.dev_community.domain.user.dto.userDto.UserInfoResponse;
+import com.likelion.dev_community.domain.user.entity.User;
+import com.likelion.dev_community.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    // 회원 정보 조회
+    public UserInfoResponse getUserInfo(Long userId){
+        User user = userRepository.findById(userId).orElseThrow(()->new CustomException(ErrorCode.NOT_FOUND, "유저를 찾을 수 없습니다. "+userId));
+
+        return UserInfoResponse.from(user);
+    }
 }
