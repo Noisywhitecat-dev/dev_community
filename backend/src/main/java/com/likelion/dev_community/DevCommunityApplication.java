c3c3cad45dbca25cf4726f60fa68d2b40083b441
KJH <kjhkmn80@gmail.com>
2026-08-05 10:37:18 +0900

    chore: UserDetailServiceAutoConfiguration 비활성화로 시큐리티가 생성하는 기본 password 제거

--- backend/src/main/java/com/likelion/dev_community/DevCommunityApplication.java
+++ backend/src/main/java/com/likelion/dev_community/DevCommunityApplication.java
@@ -2,8 +2,9 @@ package com.likelion.dev_community;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class DevCommunityApplication {
 
     public static void main(String[] args) {
