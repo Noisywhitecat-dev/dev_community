39cd59ba38263d702d9118a4659074102e0cb65d
KJH <kjhkmn80@gmail.com>
2026-08-05 20:10:42 +0900

    fix: 회원가입 요청 닉네임 @NotBlank 검증 누락 수정

--- backend/src/main/java/com/likelion/dev_community/domain/user/dto/authDto/SignUpRequest.java
+++ backend/src/main/java/com/likelion/dev_community/domain/user/dto/authDto/SignUpRequest.java
@@ -15,5 +15,6 @@ public class SignUpRequest {
     @Size(min = 8,max = 64,message = "비밀번호는 8자 이상, 64자 이하여야 합니다.")
     private final String password;
 
+    @NotBlank
     private final String nickname;
 }
